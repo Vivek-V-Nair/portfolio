dfbb6d3601ad4952c8d0d9bd2e84f5bf9c72033c
Vivek-V-Nair <vivekrokzz01@gmail.com>
2025-09-07 23:09:40 +0200

    Update to latest PHP runtime vercel-php@0.9.0 and improve Laravel setup

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
 // Vercel Serverless Function for Laravel
+// Set up the environment for Laravel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Include Laravel's bootstrap
 require __DIR__ . '/../public/index.php';
